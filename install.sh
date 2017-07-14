b2c698a965305ced1e42b8a17275a1c347a8291e
Chandrakanth <chandrakanth@shippable.com>
2017-07-14 17:39:59 +0530

    update version of python

--- install.sh
+++ install.sh
@@ -5,18 +5,19 @@ add-apt-repository -y ppa:fkrull/deadsnakes
 apt-get update
 apt-get install -y libxml2 libxml2-dev libxslt1.1 libxslt1-dev libffi-dev libssl-dev libpq-dev libmysqlclient-dev
 
-# Installing pip-8.1.2
+# Installing pip-9.0.1
 wget -nv https://bootstrap.pypa.io/3.2/get-pip.py
 python get-pip.py
 python -m pip install pip==8.1.2
 
-# Installing virtualenv-13.1.2
-wget -nv https://pypi.python.org/packages/source/v/virtualenv/virtualenv-13.1.2.tar.gz#md5=b989598f068d64b32dead530eb25589a
-tar xvzf virtualenv-13.1.2.tar.gz
-cd virtualenv-13.1.2
+pip install -U pip setuptools
+
+# Installing virtualenv-15.1.0
+wget -nv https://pypi.python.org/packages/d4/0c/9840c08189e030873387a73b90ada981885010dd9aea134d6de30cd24cb8/virtualenv-15.1.0.tar.gz#md5=44e19f4134906fe2d75124427dc9b716
+tar xvfz virtualenv-15.1.0.tar.gz
+cd virtualenv-15.1.0
 python setup.py install
 cd /
-
 #!/bin/bash
 for file in /u16pytall/version/*;
 do
