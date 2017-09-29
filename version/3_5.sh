283e7893868d6d19d5a5158ebcff19f028c12fff
Niranjan Godbole <niranjan8192@gmail.com>
2017-09-29 11:17:38 +0530

    update python 3.5 version

--- version/3_5.sh
+++ version/3_5.sh
@@ -1,9 +1,10 @@
 #!/bin/bash -e
 
 echo "================= Installing Python 3.5 ==================="
+
 sudo apt-get install -y \
-  python3.5=3.5.2-2ubuntu0~16.04.1 \
-  python3.5-dev=3.5.2-2ubuntu0~16.04.1
+  python3.5=3.5.2-2ubuntu0~16.04* \
+  python3.5-dev=3.5.2-2ubuntu0~16.04*
 
 # Install virtualenv
 virtualenv -p python3.5 $HOME/venv/3.5
