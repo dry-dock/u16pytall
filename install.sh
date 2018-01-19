4c0a9a6c5ef4db8de5690419907b467a3cf38cf8
Niranjan Godbole <niranjan8192@gmail.com>
2018-01-19 12:11:22 +0000

    use correct apt repo

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
 # install python prereqs
-add-apt-repository -y ppa:fkrull/deadsnakes
+add-apt-repository ppa:deadsnakes/ppa
 apt-get update
 apt-get install -y libxml2 libxml2-dev libxslt1.1 libxslt1-dev libffi-dev libssl-dev libpq-dev libmysqlclient-dev
 
