bc27b011b76d36868b25b62ea9a8a5cbe44cc989
vijayreddy1991 <31643910+vijayreddy1991@users.noreply.github.com>
2018-04-02 14:53:15 +0530

    Update 2_7.sh

--- version/2_7.sh
+++ version/2_7.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-echo "================= Installing Python 2.7.14 ==================="
+echo "================= Installing Python 2.7.12 ==================="
 sudo apt-get install -y \
   python2.7=2.7.12* \
   python-dev=2.7.12* \
