29e54a72c02a3f48cf077cc1ea5cd7569f6c71d5
vijayreddy1991 <31643910+vijayreddy1991@users.noreply.github.com>
2018-04-02 14:52:49 +0530

    Updated the Previous versions

--- version/2_7.sh
+++ version/2_7.sh
@@ -2,10 +2,10 @@
 
 echo "================= Installing Python 2.7.14 ==================="
 sudo apt-get install -y \
-  python2.7=2.7.14* \
-  python-dev=2.7.14* \
-  python-pip=9.0.3* \
-  python-virtualenv=15.2.0*
+  python2.7=2.7.12* \
+  python-dev=2.7.12* \
+  python-pip=8.1.2* \
+  python-virtualenv=15.1.0*
 
 # Install virtualenv
 virtualenv -p python $HOME/venv/2.7
