00f2a597e5327741c6d716d238335f4a59e86380
Ranjansingh41 <37403043+Ranjansingh41@users.noreply.github.com>
2018-05-16 12:56:23 +0530

    Create 3.1.sh

---
+++ version/3.1.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -e
+
+echo "================= Installing Python 3.1.5 ==================="
+sudo apt-get install -y \
+  python3.1=3.1* \
+  python3.1-dev=3.1*
+
+# Install virtualenv
+virtualenv -p python3.4 $HOME/venv/3.1
+
+# Install pip packages
+. $HOME/venv/3.1/bin/activate
+pip install pyopenssl ndg-httpsclient pyasn1
+pip install nose mock pytest coverage
+CFLAGS="-O0" pip install lxml
+deactivate
+echo "================= Successfully Installed Python 3.1.5 ==================="
