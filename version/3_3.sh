aef44412c099b570b1ec2f8ceee0547563c3f103
Ranjansingh41 <37403043+Ranjansingh41@users.noreply.github.com>
2018-05-16 13:59:16 +0530

    Update 3_3.sh

--- version/3_3.sh
+++ version/3_3.sh
@@ -3,7 +3,7 @@
 echo "================= Installing Python 3.3.7 ==================="
 sudo apt-get install -y \
   python3.3=3.3* \
-  python3.4-dev=3.3*
+  python3.3-dev=3.3*
 
 # Install virtualenv
 virtualenv -p python3.3 $HOME/venv/3.3
