91c0110925b71a3964fc218c24f1c88b9870fffe
Ranjansingh41 <37403043+Ranjansingh41@users.noreply.github.com>
2018-05-16 13:01:25 +0530

    Create 3_7.sh

---
+++ version/3_7.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -e
+
+echo "================= Installing Python 3.7.0 ==================="
+sudo apt-get install -y \
+  python3.7=3.7* \
+  python3.7-dev=3.7*
+
+# Install virtualenv
+virtualenv -p python3.7 $HOME/venv/3.7
+
+# Install pip packages
+. $HOME/venv/3.7/bin/activate
+pip install pyopenssl ndg-httpsclient pyasn1
+pip install nose mock pytest coverage
+CFLAGS="-O0" pip install lxml
+deactivate
+echo "================= Successfully Installed Python 3.7.0 ==================="
