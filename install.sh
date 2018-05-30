88d4ae9549168a05606c2fef271a6f3356f178c8
Ranjansingh41 <37403043+Ranjansingh41@users.noreply.github.com>
2018-05-30 17:39:43 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ wget -nv https://bootstrap.pypa.io/3.2/get-pip.py
 python get-pip.py
 python -m pip install pip==8.1.2
 
-pip install -U pip setuptools
+pip install setuptools
 
 # Installing virtualenv-15.1.0
 wget -nv https://pypi.python.org/packages/d4/0c/9840c08189e030873387a73b90ada981885010dd9aea134d6de30cd24cb8/virtualenv-15.1.0.tar.gz#md5=44e19f4134906fe2d75124427dc9b716
