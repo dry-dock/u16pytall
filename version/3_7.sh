5c580f6f32a4603aa374f7d776e2964f1a84d4bc
Ranjansingh41 <37403043+Ranjansingh41@users.noreply.github.com>
2018-07-12 12:37:21 +0530

    Update 3_7.sh

--- version/3_7.sh
+++ version/3_7.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -e
 
-echo "================= Installing Python 3.7 ==================="
+echo "================= Installing Python 3.7.0 ==================="
 sudo apt-get install -y \
-  python3.7=3.7.0 \
-  python3.7-dev=3.7.0
+  python3.7=3.7* \
+  python3.7-dev=3.7*
 
 # Install virtualenv
 virtualenv -p python3.7 $HOME/venv/3.7
@@ -12,4 +12,5 @@ virtualenv -p python3.7 $HOME/venv/3.7
 . $HOME/venv/3.7/bin/activate
 pip install nose mock pytest coverage
 deactivate
-echo "================= Successfully Installed Python 3.7 ==================="
+echo "================= Successfully Installed Python 3.7.0 ==================="
+
