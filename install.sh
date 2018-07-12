1f242dff60e5cfa543fcd74a69eb90d00321f174
Ranjansingh41 <37403043+Ranjansingh41@users.noreply.github.com>
2018-07-12 12:16:10 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -13,11 +13,8 @@ python -m pip install pip==8.1.2
 pip install setuptools
 
 # Installing virtualenv-15.1.0
-wget -nv https://pypi.python.org/packages/d4/0c/9840c08189e030873387a73b90ada981885010dd9aea134d6de30cd24cb8/virtualenv-15.1.0.tar.gz#md5=44e19f4134906fe2d75124427dc9b716
-tar xvfz virtualenv-15.1.0.tar.gz
-cd virtualenv-15.1.0
-python setup.py install
-cd /
+pip install -q virtualenv==16.0.0
+
 #!/bin/bash
 for file in /u16pytall/version/*;
 do
