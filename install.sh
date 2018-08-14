49a99f195a9c763af015fd19464080f2fda63af4
Ranjansingh41 <37403043+Ranjansingh41@users.noreply.github.com>
2018-08-14 12:14:18 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ apt-get install -y libxml2 libxml2-dev libxslt1.1 libxslt1-dev libffi-dev libssl
 # Installing pip-9.0.1
 wget -nv https://bootstrap.pypa.io/3.2/get-pip.py
 python get-pip.py
-python -m pip install pip==8.1.2
+pip install -U pip
 
 pip install setuptools
 
