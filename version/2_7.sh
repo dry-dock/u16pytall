b032d16e334aaa3488eeb98390958529b7a6dd4d
Ranjansingh41 <37403043+Ranjansingh41@users.noreply.github.com>
2018-09-18 17:26:33 +0530

    Update 2_7.sh

--- version/2_7.sh
+++ version/2_7.sh
@@ -5,7 +5,7 @@ export PYTHONDEVPKG=2.7.14*
 export PYTHONPIPPKG=8.1.1*
 export PYTHONVIRTUALENV=15.0.1+ds*
 
-echo "================= Installing $PYTHON_VERSION ==================="
+echo "================= Installing python $PYTHON_VERSION ==================="
 sudo apt-get install -y \
   python2.7="$PYTHONPKG" \
   python-dev="$PYTHONDEVPKG" \
@@ -21,7 +21,7 @@ pip install pyopenssl ndg-httpsclient pyasn1
 pip install nose mock pytest coverage
 CFLAGS="-O0" pip install lxml
 deactivate
-echo "================= Successfully Installed Python  ==================="
+echo "================= Successfully Installed Python $PYTHON_VERSION  ==================="
           
 
 
