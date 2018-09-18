0419aaa25c12484fb9c13e4544929a238c6767d9
Ranjansingh41 <37403043+Ranjansingh41@users.noreply.github.com>
2018-09-18 15:17:27 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,6 +6,10 @@ add-apt-repository ppa:deadsnakes/ppa
 apt-get update
 apt-get install -y libxml2 libxml2-dev libxslt1.1 libxslt1-dev libffi-dev libssl-dev libpq-dev libmysqlclient-dev
 
+wget -nv https://bootstrap.pypa.io/3.2/get-pip.py
+python get-pip.py
+python -m pip install --upgrade pip
+
 pip install setuptools
 
 export VIRTUALENV_VERSION=16.0.0
