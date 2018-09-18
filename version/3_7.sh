ae42a0d958cdb6567fbad86e0c524bda2d30e39f
Ranjansingh41 <37403043+Ranjansingh41@users.noreply.github.com>
2018-09-18 16:39:08 +0530

    Update 3_7.sh

--- version/3_7.sh
+++ version/3_7.sh
@@ -6,7 +6,7 @@ export PYTHONDEVPKG=3.7*
 echo "================= Installing Python "$PYTHON_VERSION" ==================="
 sudo apt-get install -y \
 python3.7="$PYTHONPKG" \
-python3.7-dev="PYTHONDEVPKG"
+python3.7-dev="$PYTHONDEVPKG"
 
 # Install virtualenv
 virtualenv -p python3.7 $HOME/venv/3.7
